9ed78f1a239dbddaf63cbb64ea2b78dbbebbe72d
Jack Kafif <107105592+jackkafif@users.noreply.github.com>
2025-07-13 10:37:04 -0500

    Create Grid with basic styling

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector("#container")
+for (let row = 0; row <= 15; row++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    for (let col = 0; col <= 15; col++) {
+        const id = `${row}${col}`;
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.id = id;
+        rowDiv.appendChild(square);
+    }
+    container.appendChild(rowDiv);
+}
